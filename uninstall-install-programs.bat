dadd860715837721eb191c616cd6db51af9cffba
girlycoded <imjoosan@gmail.com>
2026-05-02 11:14:23 -0600

    Added google drive to installers

--- uninstall-install-programs.bat
+++ uninstall-install-programs.bat
@@ -7,10 +7,9 @@ echo     - OneDrive
 echo   INSTALL:
 echo     - Git
 echo     - Firefox
+echo     - Google Drive
 echo.
-
 $ProgressPreference = 'SilentlyContinue'
-
 :: UNINSTALL ONEDRIVE
 echo [OneDrive] Uninstalling...
 taskkill /F /IM OneDrive.exe 2>nul
@@ -24,7 +23,6 @@ rd /s /q "%UserProfile%\OneDrive"            2>nul
 rd /s /q "%LocalAppData%\Microsoft\OneDrive" 2>nul
 rd /s /q "%ProgramData%\Microsoft OneDrive"  2>nul
 echo [OneDrive] Uninstalled and cleaned up.
-
 :: INSTALL GIT
 if exist "%TEMP%\git-installer.exe" (
     echo [Git] Installer already exists, launching...
@@ -32,7 +30,6 @@ if exist "%TEMP%\git-installer.exe" (
     echo [Git] Downloading...
     powershell -ExecutionPolicy Bypass -Command "$ProgressPreference = 'SilentlyContinue'; $url = (Invoke-RestMethod 'https://api.github.com/repos/git-for-windows/git/releases/latest').assets | Where-Object { $_.name -like '*64-bit.exe' } | Select-Object -ExpandProperty browser_download_url; Invoke-WebRequest $url -OutFile \"$env:TEMP\git-installer.exe\"; Write-Host '[Git] Done.'")
 start "" "%TEMP%\git-installer.exe"
-
 :: INSTALL FIREFOX
 if exist "%TEMP%\firefox-installer.exe" (
     echo [Firefox] Installer already exists, launching...
@@ -40,6 +37,12 @@ if exist "%TEMP%\firefox-installer.exe" (
     echo [Firefox] Downloading...
     powershell -ExecutionPolicy Bypass -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US' -OutFile \"$env:TEMP\firefox-installer.exe\"; Write-Host '[Firefox] Done.'")
 start "" "%TEMP%\firefox-installer.exe"
-
+:: INSTALL GOOGLE DRIVE
+if exist "%TEMP%\googledrive-installer.exe" (
+    echo [Google Drive] Installer already exists, launching...
+) else (
+    echo [Google Drive] Downloading...
+    powershell -ExecutionPolicy Bypass -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe' -OutFile \"$env:TEMP\googledrive-installer.exe\"; Write-Host '[Google Drive] Done.'")
+start "" "%TEMP%\googledrive-installer.exe"
 echo [Installers] Launched.
 pause
